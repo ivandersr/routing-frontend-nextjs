05cdb8c4b8f3f31b21eb38a88fd3a16cf776b06f
Ivander Ruiz <ivandersalvadorruiz@gmail.com>
2024-12-11 11:48:09 -0300

    feat: added websockets for `route-driver` and `admin` to update map driver locations

--- src/app/driver/MapDriver.tsx
+++ src/app/driver/MapDriver.tsx
@@ -1,11 +1,67 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useMap } from "../../hooks/useMap";
+import { socket } from "@/src/utils/socket-io";
 
-export function MapDriver() {
+export type MapDriverProps = {
+  route_id: string | null;
+  start_location: {
+    lat: number;
+    lng: number;
+  } | null;
+  end_location: {
+    lat: number;
+    lng: number;
+  } | null;
+};
+
+export function MapDriver({
+  route_id,
+  start_location,
+  end_location,
+}: MapDriverProps) {
   const mapContainerRef = useRef<HTMLDivElement>(null);
-  useMap(mapContainerRef);
+  const map = useMap(mapContainerRef);
 
-  return <div className="w-2/3 h-full" ref={mapContainerRef} />;
+  useEffect(() => {
+    if (!map || !route_id || !start_location || !end_location) {
+      return;
+    }
+
+    if (socket.disconnected) {
+      socket.connect();
+    } else {
+      socket.offAny();
+    }
+
+    socket.on("connect", () => {
+      console.log("connected to websockets");
+      socket.emit("client:new-points", { route_id });
+    });
+
+    socket.on(
+      `server:new-points/${route_id}:list`,
+      (data: { route_id: string; lat: number; lng: number }) => {
+        if (!map.hasRoute(route_id)) {
+          map.addRouteWithIcons({
+            routeId: route_id,
+            startMarkerOptions: {
+              position: start_location,
+            },
+            endMarkerOptions: {
+              position: end_location,
+            },
+            carMarkerOptions: {
+              position: start_location,
+            },
+          });
+        } else {
+          map.moveCar(route_id, { lat: data.lat, lng: data.lng });
+        }
+      }
+    );
+  }, [route_id, start_location, end_location, map]);
+
+  return <div className="w-2/3 h-screen" ref={mapContainerRef} />;
 }
